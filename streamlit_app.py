b06d24d519324bde78505c15989da6b828d2c4ac
ananyhanu <126554369+ananyhanu@users.noreply.github.com>
2023-02-28 13:34:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,6 @@ import streamlit
 
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast menu')
+streamlit.text('Sandwich and cold coffee')
+streamlit.text('chicken tikka and panner butter masala')
